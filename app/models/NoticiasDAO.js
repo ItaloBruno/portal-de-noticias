c0d7db1e1a53880fa6232d79e0a61d002e051104
ItaloBruno <italobruno_207@hotmail.com>
2018-08-18 22:18:08 -0300

    Uso do express-validator e refatoração de código usando este middleware

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -2,18 +2,18 @@ function NoticiasDAO(connection){
     this._connection = connection;
 };
 
-NoticiasDAO.prototype.getNoticias = function (callback){
+NoticiasDAO.prototype.getNoticias = function(callback){
     this._connection.query('select * from noticias', callback);
 };
 
-NoticiasDAO.prototype.getNoticia = function (callback){
+NoticiasDAO.prototype.getNoticia = function(callback){
     this._connection.query('select * from noticias where id_noticia = 2', callback);
 };
 
-NoticiasDAO.prototype.salvarNoticia = function (dadosNoticia, callback){
-    this._connection.query('insert into noticias set ?', dadosNoticia, callback);
+NoticiasDAO.prototype.salvarNoticia = function(dadosNoticia, callback){
+    this._connection.query('insert into noticias set ? ', dadosNoticia, callback);
 };
 
-module.exports = () => {
+module.exports = function(){
     return NoticiasDAO;
 };
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,14 +1,31 @@
-module.exports = (app) => {
-    app.get('/formulario_inclusao_noticia', (req, res) => {
-        res.render('admin/form_add_noticia');
+module.exports = function(app){
+    app.get('/formulario_inclusao_noticia', function(req, res){
+        res.render('admin/form_add_noticia', {validacao:{}, noticia:{}});
     });
 
-    app.post('/noticias/salvar', (req, res) => {
+    app.post('/noticias/salvar', function(req, res){
         var dadosNoticia = req.body;
+        console.log(dadosNoticia);
+
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+        req.assert('noticia', 'Notícia é obrigatória').notEmpty();
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty().isDate({format:'YYYY-MM-DD'});         
+
+        var erros = req.validationErrors();
+        console.log(erros);
+
+        if(erros){
+            res.render('admin/form_add_noticia', {validacao: erros, noticia: dadosNoticia});
+            return;
+        }
+
         var connection    = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
         
-        noticiasModel.salvarNoticia(dadosNoticia, (error, result) => {
+        noticiasModel.salvarNoticia(dadosNoticia, function(error, result){
             res.redirect('/noticias');
         });
     });
